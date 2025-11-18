a63529095801006d2537b39a8a5cd68663a596bd
carniceron <susejrobert@gmail.com>
2025-11-18 11:11:17 -0400

    redimensionado el index.php

--- templates/vis_menu.php
+++ templates/vis_menu.php
@@ -1,13 +1,18 @@
 <menu class="cont_menu">
     <nav class="nav_men">
-        <div class="us_men">
-            <div class="us_ic">A</div>
-            <b class="us_p">USER</b>
+        <div class="cont_us-red">
+            <div class="us_men">
+                <div class="us_ic">A</div>
+                <b class="us_p">USER</b>
+            </div>
+            <button class="redactar">REDACTAR</button>
         </div>
-        <button  class="redactar">REDACTAR</button>
-        <br>
-        <br>
-        <button class="tag_men tag_sended" style="background-color: #444;">Enviados (9)</button>
-        <button class="tag_men tag_fav">Destacados (0)</button>
+        <div class="butt_nav">
+            <button class="tag_men tag_sended" style="background-color: #444;">Enviados (9)</button>
+            <button class="tag_men tag_fav">Destacados (0)</button>
+            <button class="tag_men ">Papelera (0)</button>
+            <button class="tag_men ">Todos (0)</button>        
+        </div>
+        <button class="ex_sess">Cerrar sesión</button>
     </nav>
 </menu>
