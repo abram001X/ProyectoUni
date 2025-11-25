e3f6068cb18aa9ca8f1c71514c926aed955c10a4
carniceron <susejrobert@gmail.com>
2025-11-24 20:43:08 -0400

    update test

--- src/tests/getEmails.test.js
+++ src/tests/getEmails.test.js
@@ -1,10 +1,13 @@
 import { expect } from "chai";
 import { describe, it } from "vitest";
 
-function getEmails(email) {
-  
+function getEmails(email = "") {
+  const regex = /w/
+  const listEmails = email.match(regex)
 }
 
 describe("getEmails", () => {
-  
+  it("retorna una lista con los correos electronicos",()=>{
+    expect(getEmails("holasoylacra@gmail.com"))
+  })
 });
